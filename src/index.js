89596b3b4f84af54e494a6d22b7c3869568021a0
Farigus <52212018+Farigus@users.noreply.github.com>
2020-10-24 11:33:26 +0300

    feat: add mt code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+
+
+
+    if (typeof matrix !== 'object' || matrix == [] || matrix.length == 0) {
+
+        return [];
+    }
+
+
+    let newArray = [];
+
+    for (let i = 0; i < matrix.length; i++) {
+
+        if (i % 2 === 0) {
+
+            newArray = newArray.concat(matrix[i]);
+
+        } else {
+
+            newArray = newArray.concat(matrix[i].reverse());
+
+
+        }
+
+
+
+
+
+    }
+
+
+    return newArray;
+
 }
